dbad864edb810250866079b0c757145008a75a3c
Areeba Mansoor <159564271+areeba9492@users.noreply.github.com>
2024-11-10 22:44:49 +0500

    Add files via upload

---
+++ milestone1/script.js
@@ -0,0 +1,49 @@
+"use strict";
+// --------------Image section-----------------
+let img_section = document.getElementById('image');
+let imgButton = document.getElementById('imgBtn');
+imgButton?.addEventListener('click', () => {
+    img_section.classList.toggle('hide');
+});
+// --------------Contact section-----------------
+let cont_section = document.getElementById('contact');
+let contButton = document.getElementById('contBtn');
+contButton?.addEventListener('click', () => {
+    cont_section.classList.toggle('hide');
+});
+// --------------Education section-----------------
+let edu_section = document.getElementById('education');
+let eduButton = document.getElementById('eduBtn');
+eduButton?.addEventListener('click', () => {
+    edu_section.classList.toggle('hide');
+});
+// --------------language section-----------------
+let lang_section = document.getElementById('language');
+let langButton = document.getElementById('langtBtn');
+langButton?.addEventListener('click', () => {
+    lang_section.classList.toggle('hide');
+});
+// --------------Profile section-----------------
+let profile_section = document.getElementById('profile');
+let profileButton = document.getElementById('profileBtn');
+profileButton?.addEventListener('click', () => {
+    profile_section.classList.toggle('hide');
+});
+// --------------expereience section-----------------
+let exp_section = document.getElementById('experience');
+let expButton = document.getElementById('expBTn');
+expButton?.addEventListener('click', () => {
+    exp_section.classList.toggle('hide');
+});
+// --------------Skills section-----------------
+let skills_section = document.getElementById('skills');
+let skillsButton = document.getElementById('skillBtn');
+skillsButton?.addEventListener('click', () => {
+    skills_section.classList.toggle('hide');
+});
+// --------------Interest section-----------------
+let inst_section = document.getElementById('interest');
+let instButton = document.getElementById('intsBtn');
+instButton?.addEventListener('click', () => {
+    inst_section.classList.toggle('hide');
+});
